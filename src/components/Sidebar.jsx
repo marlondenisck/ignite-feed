370b3a2fdf69a594ab37e95aba35c5cce8bb0072
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2025-02-27 21:07:02 -0400

    Componente: Avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,12 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
-      <img
-        className={styles.cover}
+      <Avatar 
         src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
 
