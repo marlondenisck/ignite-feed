f10619a6796fefd94ba1e6164145b021f4a11ca3
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2025-03-01 16:08:53 -0400

    interando jsx no post

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,37 @@ import { Sidebar } from './components/Sidebar';
 import styles from './App.module.css';
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/marlondenisck.png',
+      name: 'Marlon',
+      role: 'Developer Front-end'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2024-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/marlondenisck.png',
+      name: 'Marlon',
+      role: 'Developer Front-end'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2025-03-01 07:00:00'),
+  },
+];
+
 export function App() {
   return (
     <div>
@@ -13,14 +44,16 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Diego Fernandes"
-            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
-          />
-          <Post
-            author="Gabriel Buzzi"
-            content="Um novo post muito legal"
-          />
+        {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,12 +6,13 @@ import styles from './Sidebar.module.css';
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
-      <Avatar 
+      <img
+        className={styles.cover}        
         src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/marlondenisck.png" />
+        <Avatar src="https://github.com/marlondenisck.png" />
         <strong>Marlon</strong>
         <span>Web Developer</span>
       </div>
