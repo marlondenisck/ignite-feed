7a755d46e63c6753b9c735e1ed7207e3c5e5b5eb
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2025-03-02 21:44:19 -0400

    Adiciona funcionalidade de curtidas ao componente Comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from './Comment.module.css';
 import PropTypes from 'prop-types';
 
@@ -5,8 +6,14 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
   }
 
   return (
@@ -30,9 +37,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button type="button" onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
